6660f1f9e13851cae4855494ffc969e8419d6a95
Tannin-H <50854862+Tannin-H@users.noreply.github.com>
2023-12-19 12:03:58 +1300

    lighting changes

--- display.js
+++ display.js
@@ -18,7 +18,7 @@ controls.enableDamping = true;
 // Lighting
 const ambientLight = new THREE.AmbientLight(0x404040, 4); // soft white light
 scene.add(ambientLight);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
 scene.add(directionalLight);
 
 // Resize handler
