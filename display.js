1a3ddbf8a6d583449134731d564adb2cad4ca3b2
Tannin-H <50854862+Tannin-H@users.noreply.github.com>
2023-12-18 15:48:31 +1300

    scene background colour to white

--- display.js
+++ display.js
@@ -8,6 +8,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xffffff);
 document.body.appendChild(renderer.domElement);
 
 // OrbitControls
@@ -17,7 +18,7 @@ controls.enableDamping = true;
 // Lighting
 const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(ambientLight);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 scene.add(directionalLight);
 
 // Resize handler
