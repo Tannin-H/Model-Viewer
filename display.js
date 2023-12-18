253139b43d6a002a9747f7daee3bf22bde236239
Tannin-H <50854862+Tannin-H@users.noreply.github.com>
2023-12-19 10:30:17 +1300

    stylistic changes

--- display.js
+++ display.js
@@ -8,6 +8,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xffffff); // Set background color to white
 document.body.appendChild(renderer.domElement);
 
 // OrbitControls
