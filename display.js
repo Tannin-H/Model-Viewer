359048d71c4439f6c57e307b65404141b540e4a1
Tannin-H <50854862+Tannin-H@users.noreply.github.com>
2023-12-18 16:01:31 +1300

    update

--- display.js
+++ display.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xffffff, alpha = false);
+renderer.setClearColor(#b3b3b3, alpha = false);
 document.body.appendChild(renderer.domElement);
 
 // OrbitControls
@@ -16,9 +16,9 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
 // Lighting
-const ambientLight = new THREE.AmbientLight(0x404040, 2); // soft white light
+const ambientLight = new THREE.AmbientLight(0x404040, 4); // soft white light
 scene.add(ambientLight);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
 scene.add(directionalLight);
 
 // Resize handler
