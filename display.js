c35ed5d59d766947efd133d4718ec15c5d653bb7
Tannin-H <50854862+Tannin-H@users.noreply.github.com>
2023-12-18 16:03:41 +1300

    changed alpha

--- display.js
+++ display.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(#b3b3b3, alpha = false);
+renderer.setClearColor(#b3b3b3, alpha = true);
 document.body.appendChild(renderer.domElement);
 
 // OrbitControls
