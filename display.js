e930e9c8fd43beee5cf2e3ec40dafc4b24751867
Tannin-H <50854862+Tannin-H@users.noreply.github.com>
2024-01-09 16:56:31 +1300

    Auto scaling fix

--- display.js
+++ display.js
@@ -9,7 +9,7 @@ const loadingMessage = document.getElementById('loading-message');
 
 // Scene setup
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff); // Set background color to white
@@ -50,22 +50,13 @@ function loadModel(modelData) {
             const size = new THREE.Vector3();
             boundingBox.getSize(size);
 
-            const desiredSize = 11;
+            const desiredSize = 9;
             const scaleFactor = desiredSize / size.length();
             console.log(scaleFactor);
-            object.scale.set(scaleFactor, scaleFactor, scaleFactor);
-
-            const center = new THREE.Vector3();
-            boundingBox.getCenter(center);
-
-            const centerY = scaleFactor / center.y
-            console.log(centerY)
-
-
-            camera.lookAt(center);
-            camera.position.set(0, centerY, 11)
-            controls.update();
+            
             scene.add(object);
+            object.position.set(0, 0, 0); // Adjust position if necessary
+            object.scale.set(scaleFactor, scaleFactor, scaleFactor);
             //hide loading screen once model is loaded
             loadingScreen.style.display = 'none';
 
@@ -99,7 +90,8 @@ fetch('models.json')
     })
     .catch(error => console.error('Error fetching model configurations:', error));
 
-camera.position.set(0, centerY, 5)
+// Camera position
+camera.position.z = 5;
 
 // Animation loop
 function animate() {
