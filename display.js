b13043e386f0a5b5764276ee37ba092e52d3182d
Tannin-H <50854862+Tannin-H@users.noreply.github.com>
2024-01-15 09:26:17 +1300

    auto scaling development

--- display.js
+++ display.js
@@ -49,10 +49,14 @@ function loadModel(modelData) {
             const boundingBox = new THREE.Box3().setFromObject(object);
             const size = new THREE.Vector3();
             boundingBox.getSize(size);
-                        
+            
+            const desiredSize = 9;
+            const scaleFactor = desiredSize / size.length();
+            console.log(scaleFactor);
+            object.scale.set(scaleFactor, scaleFactor, scaleFactor);
+
             scene.add(object);
             object.position.set(0, 0, 0); // Adjust position if necessary
-            object.scale.set(0.003, 0.003, 0.003)
             //hide loading screen once model is loaded
             loadingScreen.style.display = 'none';
 
